2b952b7d2209d77be21570e413e0b1a6e42bf0e7
Skripcha1 <quophiaddo633@gmail.com>
2022-12-07 15:56:18 +0000

    Second commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./Build/*.html'],
+  content: ['./Build/*.html',  './build/js/*.js'],
   theme: {
     extend: {
       screens: {
         'widescreen' : { 'raw': '(min-aspect-ratio: 3/2)'},
         'tallscreen' : { 'raw': '(min-aspect-ratio: 13/20)'},
+      },
+      keyframes: {
+        'open-menu':{
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1)' },
+          '100%': { transform: 'scaleY(0.9)' },
+        },
+        },
+        animation: {
+          'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
